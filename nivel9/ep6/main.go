776ec5b0aac4e334d5ee5c05c55eefa38e5eb3a8
Jhon Alexander <jserrano2610@gmail.com>
2020-05-05 13:59:43 -0400

    Ejercicio 6 (Falta go install)

---
+++ nivel9/ep6/main.go
@@ -0,0 +1,27 @@
+/*
+Ejercicio práctico #6
+Crea un programa que imprima tu OS y ARCH. Usa los siguientes comandos para correrlo
+    • go run
+    • go build
+    • go install
+código: https://gitlab.com/eduar/go-programming
+*/
+
+package main
+
+import (
+	"fmt"
+	"runtime"
+)
+
+func main() {
+	fmt.Print(
+		"\n************************************",
+		"\nOperating System:\t", runtime.GOOS,
+		"\nCPU Arch:\t\t", runtime.GOARCH,
+		"\nGo Version:\t\t", runtime.Version(),
+		"\nGo Path:\t\t", runtime.GOROOT(),
+		"\n************************************\n\n",
+	)
+
+}
