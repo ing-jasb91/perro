81eedf7e2ff86e3ce93ce1dbb180d1d54be8c60f
Jhon Alexander <jserrano2610@gmail.com>
2020-05-18 00:18:42 -0400

    Pequeño detalle

--- nivel10/01-channels/002-buffered/main.go
+++ nivel10/01-channels/002-buffered/main.go
@@ -1,3 +1,14 @@
+/*
+Ejercicio Práctico #1
+    • Haz que este código funcione:
+        ◦ Usando una función literal, también conocida como, función anónima autoejecutable
+            ▪ Solución: https://play.golang.org/p/SHr3lpX4so
+        ◦ Un canal con búfer
+			▪ Solución: https://play.golang.org/p/Y0Hx6IZc3U
+
+			Respuesta: https://play.golang.org/p/xO9iOSsbmWH
+*/
+
 package main
 
 import (
