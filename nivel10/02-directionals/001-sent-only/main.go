21d55cd61b911bc10ccecb95224087fb3aea9f75
Jhon Alexander <jserrano2610@gmail.com>
2020-05-18 00:34:19 -0400

    Detalle de encabezado

--- nivel10/02-directionals/001-sent-only/main.go
+++ nivel10/02-directionals/001-sent-only/main.go
@@ -1,3 +1,11 @@
+/*
+Ejercicio Práctico #2(a)
+    • Haz que este código funcione:
+        ◦ https://play.golang.org/p/oB-p3KMiH6
+			▪ Solución:  https://play.golang.org/p/isnJ8hMMKg
+Respuesta: https://play.golang.org/p/QHxrG8UEiuq
+*/
+
 package main
 
 import (
--- nivel10/02-directionals/002-receive-only/main.go
+++ nivel10/02-directionals/002-receive-only/main.go
@@ -1,3 +1,12 @@
+/*
+Ejercicio Práctico #2(a)
+    • Haz que este código funcione:
+        ◦ https://play.golang.org/p/_DBRueImEq
+			▪ Solución: https://play.golang.org/p/mgw750EPp4
+			https://play.golang.org/p/BhhgKXOYAgA
+Respuesta: https://play.golang.org/p/BhhgKXOYAgA
+*/
+
 package main
 
 import (
